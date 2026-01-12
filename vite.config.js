de202a3cd0b2b59d67e846591c28781036c92589
Edson Araujo <junioroteia@gmail.com>
2026-01-12 22:11:41 +0100

    Fix image loading by using different base paths for dev and production

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/mmfm-workshop.github.io/',
+    base: process.env.NODE_ENV === 'production' ? '/mmfm-workshop.github.io/' : '/',
 })
